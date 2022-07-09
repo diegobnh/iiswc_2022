3649709beb103fa2555e11f50d383f38937da01e
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-07-08 22:35:18 -0300

    Update generate_inputs_to_plot.sh

--- plot_scripts/generate_inputs_to_plot.sh
+++ plot_scripts/generate_inputs_to_plot.sh
@@ -9,14 +9,14 @@ rm -f input_touches_per_pages.csv
 APP_DATASET=("bc_kron" "bc_urand" "bfs_kron" "bfs_urand" "cc_kron" "cc_urand")
 
 for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
-  #-------------------------------------------------------------------------------------------------------------------
-  dram=$(grep RAM ${APP_DATASET[$j]}/autonuma/perc_access_pmem_dram_${APP_DATASET[$j]}.csv | awk -F, '{print $2}')
-  pmem=$(grep PMEM ${APP_DATASET[$j]}/autonuma/perc_access_pmem_dram_${APP_DATASET[$j]}.csv | awk -F, '{print $2}')
+ 	#-------------------------------------------------------------------------------------------------------------------
+  	dram=$(grep RAM ${APP_DATASET[$j]}/autonuma/perc_access_pmem_dram_${APP_DATASET[$j]}.csv | awk -F, '{print $2}')
+ 	pmem=$(grep PMEM ${APP_DATASET[$j]}/autonuma/perc_access_pmem_dram_${APP_DATASET[$j]}.csv | awk -F, '{print $2}')
 
 	echo ${APP_DATASET[$j]},$dram,$pmem >> input_perc_access_DRAM_and_PMEM.csv
   
-  #-------------------------------------------------------------------------------------------------------------------
-  start=$(sed -n 2p ${APP_DATASET[$j]}/autonuma/track_info_${APP_DATASET[$j]}.csv | awk -F, '{print $1}')
+  	#-------------------------------------------------------------------------------------------------------------------
+  	start=$(sed -n 2p ${APP_DATASET[$j]}/autonuma/track_info_${APP_DATASET[$j]}.csv | awk -F, '{print $1}')
 	end=$(tail -n 1 ${APP_DATASET[$j]}/autonuma/track_info_${APP_DATASET[$j]}.csv | awk -F, '{print $1}')
 	exec_time_autonuma=$(echo $start $end | awk '{print ($2-$1)/60}')
 
@@ -27,11 +27,11 @@ for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
 	echo -n ${APP_DATASET[$j]},$exec_time_autonuma,$exec_time_static_mapping, >> input_exec_time.csv
 	echo $exec_time_static_mapping $exec_time_autonuma | awk '{print (1-($1/$2))*100}' >> input_exec_time.csv
   
-  #-------------------------------------------------------------------------------------------------------------------
-  one_touch=$(grep "1 touch" ${APP_DATASET[$j]}/autonuma/df_touch_per_page_${APP_DATASET[$j]}.csv | awk -F, '{print $2}')
-  two_touches=$(grep "2 touches" ${APP_DATASET[$j]}/autonuma/df_touch_per_page_${APP_DATASET[$j]}.csv | awk -F, '{print $2}')
+	#-------------------------------------------------------------------------------------------------------------------
+	one_touch=$(grep "1 touch" ${APP_DATASET[$j]}/autonuma/df_touch_per_page_${APP_DATASET[$j]}.csv | awk -F, '{print $2}')
+	two_touches=$(grep "2 touches" ${APP_DATASET[$j]}/autonuma/df_touch_per_page_${APP_DATASET[$j]}.csv | awk -F, '{print $2}')
 
 	echo ${APP_DATASET[$j]},$one_touch,$two_touches >> input_touches_per_pages.csv
   
-  #-------------------------------------------------------------------------------------------------------------------
+	#-------------------------------------------------------------------------------------------------------------------
 done
