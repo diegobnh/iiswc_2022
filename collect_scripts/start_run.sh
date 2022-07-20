43dd75ee9300d83106195c202bea091f88e95873
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-07-20 18:47:01 -0300

    Create start_run.sh

---
+++ collect_scripts/start_run.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+APP_DATASET=("bc_kron" "bc_urand" "bfs_kron" "bfs_urand" "cc_kron" "cc_urand")
+APP=("bc" "bc" "bfs" "bfs" "cc" "cc")
+DATASET=("kron" "urand" "kron" "urand" "kron" "urand")
+
+
+for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
+        echo "Running:"${APP_DATASET[$j]}
+
+        sudo -u dmoura mkdir -p ${APP_DATASET[$j]}
+        sudo -u dmoura mkdir -p ${APP_DATASET[$j]}/autonuma
+        sudo -u dmoura chmod +777 ${APP_DATASET[$j]}/autonuma
+
+	cd ${APP_DATASET[$j]}/autonuma
+        rm -f *
+
+        cp ../../run.sh .
+        cp ../../shared_library/mmap_intercept_only_to_trace.so .
+
+        sudo ./run.sh ${APP[$j]} ${DATASET[$j]} autonuma
+
+        rm run.sh *.so call_stack.txt
+        cd ../..
+done
+
+
+for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
+        echo "Running:"${APP_DATASET[$j]}
+
+        sudo -u dmoura mkdir -p ${APP_DATASET[$j]}/static_mapping
+        sudo -u dmoura chmod +777 ${APP_DATASET[$j]}/static_mapping
+
+        cd ${APP_DATASET[$j]}/static_mapping
+        rm -f *
+
+        cp ../../run.sh .
+        cp ../../shared_library/mmap_intercept_to_static_bind.so .
+
+        sudo ./run.sh ${APP[$j]} ${DATASET[$j]} static_mapping
+
+        rm run.sh *.so call_stack.txt
+        cd ../..
+done
