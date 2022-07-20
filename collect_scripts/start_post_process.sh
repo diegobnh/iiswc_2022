0bb736846a50e72ce39e988212f34e05e9d96cc7
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-07-20 18:47:50 -0300

    Create start_post_process.sh

---
+++ collect_scripts/start_post_process.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+APP_DATASET=("bc_kron" "bc_urand" "bfs_kron" "bfs_urand" "cc_kron" "cc_urand")
+APP=("bc" "bc" "bfs" "bfs" "cc" "cc")
+DATASET=("kron" "urand" "kron" "urand" "kron" "urand")
+
+
+for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
+        echo "Running:"${APP_DATASET[$j]}
+
+	cd ${APP_DATASET[$j]}/autonuma
+        cp ../../post_process.sh .
+
+        sudo ./post_process.sh ${APP[$j]} ${APP_DATASET[$j]}
+
+        rm post_process.sh
+        cd ../..
+done
+
+for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
+        echo "Running:"${APP_DATASET[$j]}
+
+        cd ${APP_DATASET[$j]}/static_mapping
+        cp ../../post_process.sh .
+
+        sudo ./post_process.sh ${APP[$j]} ${APP_DATASET[$j]}
+
+        rm post_process.sh run.sh
+        cd ../..
+done
