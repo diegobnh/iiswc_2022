d0d492fa2c71d8f51587dc20383f7dfec75104ef
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-09-04 16:13:17 -0300

    Update start_post_process.sh

--- collect_scripts/start_post_process.sh
+++ collect_scripts/start_post_process.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-APP_DATASET=("bc_kron" "bc_urand" "bfs_kron" "bfs_urand" "cc_kron" "cc_urand")
-APP=("bc" "bc" "bfs" "bfs" "cc" "cc")
-DATASET=("kron" "urand" "kron" "urand" "kron" "urand")
+source app_dataset.sh
 
-    
 for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
     echo "Running:"${APP_DATASET[$j]}
 
