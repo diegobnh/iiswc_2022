514101fc5e2aa7e08d350f13fbffff2d4ad10722
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-09-04 14:17:58 -0300

    Update start_post_process.sh

--- collect_scripts/start_post_process.sh
+++ collect_scripts/start_post_process.sh
@@ -4,27 +4,27 @@ APP_DATASET=("bc_kron" "bc_urand" "bfs_kron" "bfs_urand" "cc_kron" "cc_urand")
 APP=("bc" "bc" "bfs" "bfs" "cc" "cc")
 DATASET=("kron" "urand" "kron" "urand" "kron" "urand")
 
-
+    
 for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
-        echo "Running:"${APP_DATASET[$j]}
+    echo "Running:"${APP_DATASET[$j]}
 
-	cd ${APP_DATASET[$j]}/autonuma
-        cp ../../post_process.sh .
+    cd ${APP_DATASET[$j]}/autonuma
+    cp ../../post_process.sh .
 
-        sudo ./post_process.sh ${APP[$j]} ${APP_DATASET[$j]}
+    sudo ./post_process.sh ${APP[$j]} ${APP_DATASET[$j]}
 
-        rm post_process.sh
-        cd ../..
+    rm post_process.sh
+    cd ../..
 done
 
 for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
-        echo "Running:"${APP_DATASET[$j]}
+    echo "Running:"${APP_DATASET[$j]}
 
-        cd ${APP_DATASET[$j]}/static_mapping
-        cp ../../post_process.sh .
+    cd ${APP_DATASET[$j]}/static_mapping
+    cp ../../post_process.sh .
 
-        sudo ./post_process.sh ${APP[$j]} ${APP_DATASET[$j]}
+    sudo ./post_process.sh ${APP[$j]} ${APP_DATASET[$j]}
 
-        rm post_process.sh run.sh
-        cd ../..
+    rm post_process.sh run.sh
+    cd ../..
 done
