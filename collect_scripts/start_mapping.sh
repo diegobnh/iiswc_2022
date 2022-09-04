332a506049b7fb9f33adca9599486279a4857826
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-09-04 14:39:02 -0300

    Create start_mapping.sh

---
+++ collect_scripts/start_mapping.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+APP_DATASET=("bc_kron" "bfs_kron" "bfs_urand" "cc_kron" "cc_urand")
+
+for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
+    echo "Running:"${APP_DATASET[$j]}
+    #cd ${APP_DATASET[$j]}/autonuma
+    cd ${APP_DATASET[$j]}/static_mapping/
+    time python3 -W ignore /ix/dmosse/dmoura/iiswc_2022/mapping.py $(pwd)
+    cd ../..
+done
