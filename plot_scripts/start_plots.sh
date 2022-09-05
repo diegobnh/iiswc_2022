404b13f1c5d9ba704033f5adf224f5c4ff6987c8
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-09-05 09:54:19 -0300

    Update start_plots.sh

--- plot_scripts/start_plots.sh
+++ plot_scripts/start_plots.sh
@@ -3,13 +3,12 @@
 source ../collect_scripts/app_dataset.sh
 
 for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
-        echo "Running:"${APP_DATASET[$j]}
-        #cd ${APP_DATASET[$j]}/autonuma
-        cd ${APP_DATASET[$j]}/static_mapping/
+    echo "Running:"${APP_DATASET[$j]}
+    #cd ${APP_DATASET[$j]}/autonuma
+    cd ${APP_DATASET[$j]}/static_mapping/
 
-        time python3 -W ignore /ix/dmosse/dmoura/iiswc_2022/plots.py
-        mkdir plots
-        mv *.pdf plots
-
-        cd ../..
+    time python3 -W ignore /ix/dmosse/dmoura/iiswc_2022/plots.py
+    mkdir plots
+    mv *.pdf plots
+    cd ../..
 done
