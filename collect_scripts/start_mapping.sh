510e7e05c4cdec4f85f93437cc6e35ea1c149432
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-09-05 07:49:20 -0300

    Update start_mapping.sh

--- collect_scripts/start_mapping.sh
+++ collect_scripts/start_mapping.sh
@@ -4,7 +4,8 @@ source app_dataset.sh
 
 for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
     echo "Running:"${APP_DATASET[$j]}
-    #cd ${APP_DATASET[$j]}/autonuma
+    cd ${APP_DATASET[$j]}/autonuma
+    cd ../..
     cd ${APP_DATASET[$j]}/static_mapping/
     time python3 -W ignore ../../mapping.py $(pwd)
     cd ../..
