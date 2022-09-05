64ec2b83224a1b493e27b91c34cff2c8cdb5f685
diegobnh <37424347+diegobnh@users.noreply.github.com>
2022-09-05 07:48:28 -0300

    Update start_mapping.sh

--- collect_scripts/start_mapping.sh
+++ collect_scripts/start_mapping.sh
@@ -6,6 +6,6 @@ for ((j = 0; j < ${#APP_DATASET[@]}; j++)); do
     echo "Running:"${APP_DATASET[$j]}
     #cd ${APP_DATASET[$j]}/autonuma
     cd ${APP_DATASET[$j]}/static_mapping/
-    time python3 -W ignore /ix/dmosse/dmoura/iiswc_2022/mapping.py $(pwd)
+    time python3 -W ignore ../../mapping.py $(pwd)
     cd ../..
 done
